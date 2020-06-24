5fcee7708a322552184ae5077cc1844c655feb68
HyeonSeong-Bae <HyeonSeong-Bae@stradvision.com>
2020-06-24 15:40:11 +0900

    Print texture mapping

--- source.cpp
+++ source.cpp
@@ -12,6 +12,10 @@
 
 using namespace std;
 
+GLuint CreateTexture(char const* filename);
+bool initShaderProgram();
+bool defineTextureObject();
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void errorCallback(int errorCode, const char* errorDescription);
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
@@ -19,198 +23,7 @@ void renderScene(GLFWwindow* window);
 
 int framebufferWidth, framebufferHeight;
 GLuint g_VAO, g_VBO, g_EBO;
-GLuint g_programID;
-GLuint trianglePositionVertexBufferObjectID, triangleColorVertexBufferObjectID;
-GLuint triangleTextureCoordinateBufferObjectID;
-
-// Method to load an image into a texture using the freeimageplus library. Returns the texture ID or dies trying.
-GLuint CreateTexture(char const* filename)
-{
-  // load image
-  int width, height, channel;
-
-  stbi_set_flip_vertically_on_load(true);
-
-  GLubyte* textureData = stbi_load(filename, &width, &height, &channel, 0);
-
-  // Generate a texture ID and bind to it
-  GLuint tempTextureID;
-  glGenTextures(1, &tempTextureID);
-  glBindTexture(GL_TEXTURE_2D, tempTextureID);
-
-  // Construct the texture.
-  // Note: The 'Data format' is the format of the image data as provided by the image library. FreeImage decodes images into
-  // BGR/BGRA format, but we want to work with it in the more common RGBA format, so we specify the 'Internal format' as such.
-  glTexImage2D(GL_TEXTURE_2D,    // Type of texture
-    0,                // Mipmap level (0 being the top level i.e. full size)
-    GL_RGB,          // Internal format
-    width,       // Width of the texture
-    height,      // Height of the texture,
-    0,                // Border in pixels
-    GL_BGR,          // Data format
-    GL_UNSIGNED_BYTE, // Type of texture data
-    textureData);     // The image data to use for this texture
-
-              // Specify our minification and magnification filters
-
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-  glBindTexture(GL_TEXTURE_2D, 0);
-
-  return tempTextureID;
-}
-
-bool initShaderProgram() {
-
-  //load and compile shaders
-
-  //char* vertex_shader = (char*)"Shaders\\vertex_shader.glsl";
-  //char* fragment_shader = (char*)"Shaders\\fragment_shader.glsl";
-  //Core::Shader_Loader shaderLoader;
-  //g_programID = shaderLoader.CreateProgram(vertex_shader, fragment_shader);
-
-  //#3
-  const GLchar* vertexShaderSource =
-    "#version 330 core\n"
-    "in vec3 positionAttribute;"
-    "in vec2 textureCoordinateAttribute;"
-    "out vec2 passTextureCoordinateAttribute;"
-    "void main()"
-    "{"
-    "gl_Position = vec4(positionAttribute, 1.0);"
-    "passTextureCoordinateAttribute = textureCoordinateAttribute;"
-    "}";
-
-
-  //#4
-  const GLchar* fragmentShaderSource =
-    "#version 330 core\n"
-    "in vec2 passTextureCoordinateAttribute;"
-    "out vec4 fragmentColor;"
-    "uniform sampler2D tex;"
-    "void main()"
-    "{"
-    "fragmentColor = texture(tex, passTextureCoordinateAttribute);"
-     "}";
-
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-  glCompileShader(vertexShader);
-
-  GLint result;
-  GLchar errorLog[512];
-  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-  if (!result)
-  {
-    glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-    cerr << "ERROR: vertex shader result error\n" << errorLog << endl;
-    glDeleteShader(vertexShader);
-    return false;
-  }
-
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-  glCompileShader(fragmentShader);
-
-  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-  if (!result)
-  {
-    glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-    cerr << "ERROR: fragment shader result error\n" << errorLog << endl;
-
-    return false;
-  }
-
-  //#5
-  g_programID = glCreateProgram();
-
-  glAttachShader(g_programID, vertexShader);
-  glAttachShader(g_programID, fragmentShader);
-
-  glLinkProgram(g_programID);
-
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
-
-  glGetProgramiv(g_programID, GL_LINK_STATUS, &result);
-  if (!result) {
-    glGetProgramInfoLog(g_programID, 512, NULL, errorLog);
-    cerr << "ERROR: shader program result error\n" << errorLog << endl;
-    return false;
-  }
-
-  return true;
-}
-
-bool defineTextureObject() {
-
-  //#1
-  float position[] = {
-    0.0f,  0.5f, 0.0f, //vertex 1  
-    0.5f, -0.5f, 0.0f, //vertex 2  
-    -0.5f, -0.5f, 0.0f //vertex 3  
-  };
-
-  float color[] = {
-    1.0f, 0.0f, 0.0f, //vertex 1 : RED (1,0,0)
-    0.0f, 1.0f, 0.0f, //vertex 2 : GREEN (0,1,0) 
-    0.0f, 0.0f, 1.0f  //vertex 3 : BLUE (0,0,1)
-  };
-
-  float textureCoordinate[] = {
-    0.5f, 1.0f,  //vertex 1  
-    1.0f, 0.0f,  //vertex 2
-    0.0f, 0.0f   //vertex 3        
-  };
-
-  //#2
-  //Vertex Buffer Object(VBO)
-  glGenBuffers(1, &trianglePositionVertexBufferObjectID);
-  glBindBuffer(GL_ARRAY_BUFFER, trianglePositionVertexBufferObjectID);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &triangleColorVertexBufferObjectID);
-  glBindBuffer(GL_ARRAY_BUFFER, triangleColorVertexBufferObjectID);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
-
-  glGenBuffers(1, &triangleTextureCoordinateBufferObjectID);
-  glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinate), textureCoordinate, GL_STATIC_DRAW);
-
-  //#6
-  glGenVertexArrays(1, &g_VAO);
-  glBindVertexArray(g_VAO);
-
-
-  GLint positionAttribute = glGetAttribLocation(g_programID, "positionAttribute");
-  if (positionAttribute == -1) {
-    cerr << "position attribute error" << endl;
-    return false;
-  }
-  glBindBuffer(GL_ARRAY_BUFFER, trianglePositionVertexBufferObjectID);
-  glVertexAttribPointer(positionAttribute, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-  glEnableVertexAttribArray(positionAttribute);
-
-  GLint textureCoordinateAttribute = glGetAttribLocation(g_programID, "textureCoordinateAttribute");
-  if (textureCoordinateAttribute == -1) {
-    cerr << "Texture Coordinate attribute error" << endl;
-    return false;
-  }
-  glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID);
-  glVertexAttribPointer(textureCoordinateAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(textureCoordinateAttribute);
-
-  glBindVertexArray(0);
-
-  return true;
-}
-
-
+GLuint g_shaderProgramID;
 
 int main()
 {
@@ -230,8 +43,8 @@ int main()
   glfwWindowHint(GLFW_SAMPLES, 4);
 
   GLFWwindow* window = glfwCreateWindow(
-    800,
-    600,
+    1280,
+    720,
     "OpenGL Example",
     NULL, NULL);
   if (!window) {
@@ -268,14 +81,6 @@ int main()
   cout << "Vendor: " << glGetString(GL_VENDOR) << endl;
   cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
 
-  if (!initShaderProgram()) {
-
-    cerr << "Error: Shader Program init error" << endl;
-
-    glfwTerminate();
-    std::exit(EXIT_FAILURE);
-  }
-
   if (!defineTextureObject()) {
 
     cerr << "Error: Shader Program define defineTextureObject error" << endl;
@@ -284,17 +89,18 @@ int main()
     std::exit(EXIT_FAILURE);
   }
 
+  if (!initShaderProgram()) {
+
+    cerr << "Error: Shader Program init error" << endl;
+
+    glfwTerminate();
+    std::exit(EXIT_FAILURE);
+  }
+
   glfwSwapInterval(1);
 
-  glUseProgram(g_programID);
-  glBindVertexArray(g_VAO);
-
   GLuint texureId = CreateTexture("C:/data/test.jpg");
-
-  glUniform1i(glGetUniformLocation(g_programID, "tex"), 0);
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_2D, texureId);
-
+  
   while (!glfwWindowShouldClose(window)) {
     renderScene(window);
   }
@@ -302,10 +108,10 @@ int main()
   glUseProgram(0);
   glBindVertexArray(0);
 
-  glDeleteProgram(g_programID);
-  glDeleteBuffers(1, &trianglePositionVertexBufferObjectID);
-  glDeleteBuffers(1, &triangleColorVertexBufferObjectID);
-  glDeleteBuffers(1, &triangleTextureCoordinateBufferObjectID);
+  glDeleteTextures(1, &texureId);
+  glDeleteProgram(g_shaderProgramID);
+  glDeleteBuffers(1, &g_EBO);
+  glDeleteBuffers(1, &g_VBO);
   glDeleteVertexArrays(1, &g_VAO);
   glfwTerminate();
 
@@ -331,13 +137,191 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
+GLuint CreateTexture(char const* filename)
+{
+  // load image
+  int width, height, channel;
+
+  stbi_set_flip_vertically_on_load(true);
+
+  GLubyte* textureData = stbi_load(filename, &width, &height, &channel, STBI_rgb);
+
+  // Generate a texture ID and bind to it
+  GLuint tempTextureID;
+  glGenTextures(1, &tempTextureID);
+  glBindTexture(GL_TEXTURE_2D, tempTextureID);
+
+  // Construct the texture.
+  // Note: The 'Data format' is the format of the image data as provided by the image library. FreeImage decodes images into
+  // BGR/BGRA format, but we want to work with it in the more common RGBA format, so we specify the 'Internal format' as such.
+  glTexImage2D(GL_TEXTURE_2D,    // Type of texture
+    0,                // Mipmap level (0 being the top level i.e. full size)
+    GL_RGB,          // Internal format
+    width,       // Width of the texture
+    height,      // Height of the texture,
+    0,                // Border in pixels
+    GL_RGB,          // Data format
+    GL_UNSIGNED_BYTE, // Type of texture data
+    textureData);     // The image data to use for this texture
+
+              // Specify our minification and magnification filters
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  stbi_image_free(textureData);
+
+  return tempTextureID;
+}
+
 void renderScene(GLFWwindow* window)
 {
-  glClearColor(0, 0, 0, 1);
+  glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
   glfwSwapBuffers(window);
   glfwPollEvents();
+}
+
+bool initShaderProgram() {
+
+  //load and compile shaders
+
+  //char* vertex_shader = (char*)"Shaders\\vertex_shader.glsl";
+  //char* fragment_shader = (char*)"Shaders\\fragment_shader.glsl";
+  //Core::Shader_Loader shaderLoader;
+  //g_shaderProgramID = shaderLoader.CreateProgram(vertex_shader, fragment_shader);
+
+  const GLchar* vertexShaderSource =
+    "#version 330 core\n"
+    "in vec3 aPos;"
+    "in vec3 aColor;"
+    "in vec2 aTexCoord;"
+    "out vec3 Color;"
+    "out vec2 TexCoord;"
+    "void main()"
+    "{"
+    "gl_Position = vec4(aPos, 1.0F);"
+    "Color = aColor;"
+    "TexCoord = aTexCoord;"
+    "}";
+
+  const GLchar* fragmentShaderSource =
+    "#version 330 core\n"
+    "in vec3 Color;"
+    "in vec2 TexCoord;"
+    "out vec4 outColor;"
+    "uniform sampler2D ourTexture;"
+    "void main()"
+    "{"
+    //"outColor = texture(tex, TexCoord) * vec4(Color, 1.0);" 
+    "outColor = texture(ourTexture, TexCoord) * vec4(Color, 1.0);"
+    "}";
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  GLint result;
+  GLchar errorLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+  if (!result)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+    cerr << "ERROR: vertex shader result error\n" << errorLog << endl;
+    glDeleteShader(vertexShader);
+    return false;
+  }
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+  if (!result)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+    cerr << "ERROR: fragment shader result error\n" << errorLog << endl;
+
+    return false;
+  }
+
+  // Link the vertex and fragment shader into a shader program
+  g_shaderProgramID = glCreateProgram();
+  glAttachShader(g_shaderProgramID, vertexShader);
+  glAttachShader(g_shaderProgramID, fragmentShader);
+  glBindFragDataLocation(g_shaderProgramID, 0, "outColor");
+  glLinkProgram(g_shaderProgramID);
+  glUseProgram(g_shaderProgramID);
+
+  // specify the layout of the vertex data
+  GLint posAttrib = glGetAttribLocation(g_shaderProgramID, "aPos");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
+  if (posAttrib == -1) {
+    cerr << "position attribute error" << endl;
+    return false;
+  }
+
+  GLint colAttrib = glGetAttribLocation(g_shaderProgramID, "aColor");
+  glEnableVertexAttribArray(colAttrib);
+  glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+  if (colAttrib == -1) {
+    cerr << "position attribute error" << endl;
+    return false;
+  }
+
+  GLint texAttrib = glGetAttribLocation(g_shaderProgramID, "aTexCoord");
+  glEnableVertexAttribArray(texAttrib);
+  glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+  if (texAttrib == -1) {
+    cerr << "position attribute error" << endl;
+    return false;
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  glGetProgramiv(g_shaderProgramID, GL_LINK_STATUS, &result);
+  if (!result) {
+    glGetProgramInfoLog(g_shaderProgramID, 512, NULL, errorLog);
+    cerr << "ERROR: shader program result error\n" << errorLog << endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool defineTextureObject() {
+
+  float vertices[] = {
+    // positions          // colors           // texture coords
+      0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // top right
+      0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // top left 
+  };
+
+  unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
+
+  glGenVertexArrays(1, &g_VAO);
+  glGenBuffers(1, &g_VBO);
+  glGenBuffers(1, &g_EBO);
+
+  glBindVertexArray(g_VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, g_VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  return true;
 }
